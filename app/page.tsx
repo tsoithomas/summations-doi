0cda36527f5c2c59d48691a37e2bc59b86556dc8
Thomas Tsoi <tsoithomas@outlook.com>
2023-07-25 04:42:26 -0400

    Fixed minor bug (class -> className)

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
           <div className="my-2">
             <DoiInput />
           </div>
-          <button class="bg-teal-500 hover:bg-teal-700 border text-white font-bold py-2 px-4 rounded inline-flex items-center cursor-pointer">
-            <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+          <button className="bg-teal-500 hover:bg-teal-700 border text-white font-bold py-2 px-4 rounded inline-flex items-center cursor-pointer">
+            <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
             <span>Retrieve</span>
           </button>
         </form>
